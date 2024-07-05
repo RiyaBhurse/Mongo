f90c38622b96285dd2c2a7258751413d8fa00312
riya bhurse <riyabhurse.rb@gmail.com>
2024-07-05 11:51:07 +0530

    added post in db

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
 mongoose
     .connect('mongodb+srv://riyabhurserb:1jlXqmsxxytluM1h@cluster0.zr1nnc4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => {
@@ -7,10 +9,7 @@ mongoose
     }).catch((err) => {  
         console.log('Failed to connect to MongoDB', err);
     });
-const app = express();
-app.listen(8080, () => {    
-    console.log('Yay!!!Server started on port 8080');
-});
+
 
 //Product Schema
 const productSchema = new mongoose.Schema({ 
@@ -30,4 +29,20 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: true
     }
+});
+
+const productModel = mongoose.model('Products', productSchema);
+app.post('/api/products', async (req, res) => {
+    const body = req.body;
+    const product = productModel.create({
+        name: body.name,
+        price: body.price,
+        isInStock: body.isInStock,
+        category: body.category
+    });
+    console.log(product);
+    return res.status(201).json({message : "Product created successfully"});
+});
+app.listen(8002, () => {    
+    console.log('Yay!!!Server started on port 8002');
 });
